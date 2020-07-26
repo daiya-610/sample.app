db422406de8879502c0865f1197ccce32b68d889
daiya <daiya06010@gmail.com>
2020-07-26 23:11:20 +0900

    Update application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -19,7 +20,8 @@ class UsersController < ApplicationController
 
   private
 
-   def user_params
-     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-   end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
+      post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
-                                         password:             "foo",
-                                         passwprd_confirmation: "bar"} }
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#<CSS id for error explanation>'
